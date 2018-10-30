d1facd6de1de52bcfa72910adf0ab426c202cff9
harshpeswani45 <harshpeswani45@gmail.com>
2018-10-30 10:25:16 +0530

    Concate Without Using Function

---
+++ concate.js
@@ -0,0 +1,15 @@
+
+    var a=[1,2,3,4]
+    var b=[5,6,7,8]
+    var c=[]
+    var i,j
+    for(i=0;i<a.length;i++)
+    {
+        c[i]=a[i]
+    }
+    for(j=0;j<b.length;j++)
+    {
+        c[i]=b[j]
+        i++
+    }
+    console.log(c)
