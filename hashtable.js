1a19b2c625890106a4fcead995e585276d0ebd50
harshpeswani45 <harshpeswani45@gmail.com>
2018-11-05 15:56:10 +0530

    hashtable.js

--- hashtable.js
+++ hashtable.js
@@ -1,12 +1,22 @@
 
-    var a=['a','a','a','a','a','a','a','a','a','a','v','v']
-    var count={'a':0,'v':0}
+    var a=['a','j','p','>','a']
+    var count={}
     var i,element
     for(i in a)
     {
+        var flag=0
         for(element in count)
-            if(element==a[i])
+        {
+            if(element==a[i]){
+
                 count[element]++
+                flag=1
+            }
+        }
+        if(flag==0)
+        {
+          count[a[i]]=1
+        }
     }
     for(element in count)
     {
